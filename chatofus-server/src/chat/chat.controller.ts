e1c6dc718400b49336c4d86cf3ab2a74fc160f67
littleone <nathanael.constant@gmail.com>
2024-12-28 14:46:49 +0100

    updated http post

--- chatofus-server/src/chat/chat.controller.ts
+++ chatofus-server/src/chat/chat.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {Body, Controller, Post, Headers, Req, RawBodyRequest} from '@nestjs/common';
 import { ChatService } from './chat.service';
 
 @Controller('chat')
@@ -7,15 +7,25 @@ export class ChatController {
     constructor(private readonly chatService : ChatService) {};
 
     // @Post()
-    // addChatMessage(@Body() chatMessage): void {
-    //     console.log('addChatMessage()');
-    //     console.log('Body :', chatMessage);
-    //     this.chatService.addChatMessage(chatMessage);
+    // addChatMessage(@Headers('Content-Type') contentType: string, @Headers('Key') key: string, @Headers('ProtoMessageName') protoMessageName: string, @Body() body: any) {
+    //     console.log('Le type : ', contentType);
+    //     console.log('La clé : ', key);
+    //     console.log('Le type Message : ', protoMessageName);
+    //     console.log('Le body : ', body);
+    //
+    //     // this.chatService.addChatMessage();
     // }
 
     @Post()
-    addChatMessage(): void {
-        this.chatService.addChatMessage();
+    addChatMessage(@Req() request: any) {
+        console.log('Le type : ', request.headers['content-type']);
+        console.log('La clé : ', request.headers['key']);
+        console.log('Le type Message : ', request.headers['protomessagename']);
+        console.log('Le raw body : ', request.rawBody);
+        console.log('Le body : ', request.body);
+        // console.log('REQUEST : ', request)
+
+        // this.chatService.addChatMessage();
     }
 
 
