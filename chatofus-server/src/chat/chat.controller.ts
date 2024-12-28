2ec43fa675d839180241d5861f9f718dea981003
naconstant <nathanael.constant2@soprasteria.com>
2024-12-28 18:01:58 +0100

    translate jspn to dto for chat data

--- chatofus-server/src/chat/chat.controller.ts
+++ chatofus-server/src/chat/chat.controller.ts
@@ -3,20 +3,10 @@ import { ChatService } from './chat.service';
 
 @Controller('chat')
 export class ChatController {
+  constructor(private readonly chatService: ChatService) {}
 
-    constructor(private readonly chatService : ChatService) {};
-
-    // @Post()
-    // addChatMessage(@Body() chatMessage): void {
-    //     console.log('addChatMessage()');
-    //     console.log('Body :', chatMessage);
-    //     this.chatService.addChatMessage(chatMessage);
-    // }
-
-    @Post()
-    addChatMessage(): void {
-        this.chatService.addChatMessage();
-    }
-
-
+  @Post()
+  addChatMessage(@Body() body: string): void {
+    this.chatService.handleChatData(body);
+  }
 }
